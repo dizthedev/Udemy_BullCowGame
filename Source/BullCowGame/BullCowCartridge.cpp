129bc63e9ce3ae141a7f09446df187755c37766a
diz <dizn94@gmail.com>
2021-02-26 01:48:40 -0500

    SetupGame() in BCC.h and .cpp

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -6,20 +6,50 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     Super::BeginPlay();
 	PrintLine(TEXT("Welcome to Bull Cows!\nGuess the 4 letter word!"));
 	PrintLine(TEXT("Press enter to continue...")); 
-	//separate lines because they're stating two different kind of things: A welcome, and in instruction
-	HiddenWord = TEXT("word");
+	
+	InitGame();
+	
+	//Prompt Player for guess
 
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
 	ClearScreen();
+
 	PrintLine(TEXT("You entered: " + Input));
-	
+
 	if (Input == HiddenWord)
 	{
 		PrintLine("YOU WIN! How did you guess that?");
 	}
 	else
+	{
 		PrintLine("YOU LOSE! HAHA");
+		Lives--;
+		PrintLine("Lives = " + FString::FromInt(Lives));
+	}
+
+	//Check if Isogram
+
+	//Check Right Number of Characters
+
+	//Remove Life
+
+	//Check if lives > 0
+	//if yes, guess again
+	//show remaining lives
+	//if no, game over and show hidden word
+	//prompt to play again
+	//yes restart
+	//no quit
+}
+
+void UBullCowCartridge::InitGame()
+{
+	//setting up game
+	HiddenWord = TEXT("word");
+
+	//set lives
+	Lives = 10;
 }
